c876d646228e5596f734e03293525d6525feb460
Cara Steffen <cara@treadstonedesign.com>
2020-04-25 13:18:32 -0600

    adding javascript file

---
+++ portfolio.js
@@ -0,0 +1,41 @@
+function careerReturnship() {
+  <div class="projectContainer">
+    <div class="project pro2">
+      <h4>Career Returnship Website</h4>
+      <i>A group project for Denver/Boulder Women Who Code Meetup Group</i>
+      <br />
+      <a href="https://cssteffen.github.io/Go-Be_Active-API/" target="_blank">
+        <img
+          class="projectImage"
+          src="https://user-images.githubusercontent.com/30050423/62484557-f76d4400-b7a9-11e9-92c7-23efa5be14f4.png"
+          alt="image of Go-Be Active API Web app"
+        />
+      </a>
+      <p>
+        Re-entering the tech-force after a break is hard. Together we can make
+        it easier. Current website being built for WWC Career-Returnship meetup
+        group. I am
+      </p>
+      <p>Building using Figma, Adobe Photoshop, HTML, CSS, JavaScript.</p>
+      <div class="projectLinks">
+        <p class="gitLinks">
+          <a
+            class="portfolioLinks"
+            href="https://github.com/cssteffen/Go-Be_Active-API"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            class="portfolioLinks"
+            href="https://cssteffen.github.io/Go-Be_Active-API/"
+            target="_blank"
+          >
+            Demo
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>;
+}
+careerReturnship();
